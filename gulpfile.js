21be119412e9f7fd9a02f719cc53cbae74715972
Angus Palmer <angus.palmer@ac3.com.au>
2016-08-17 23:25:44 +1000

    Added JSON watch task

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,10 @@ var jsSources = [
 
 var sassSources = ['components/sass/style.scss'];
 
+var htmlSources = ['builds/development/*.html'];
+
+var jsonSources = ['builds/development/js/*.json'];
+
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true})
@@ -51,6 +55,8 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['sass']);
+	gulp.watch(jsonSources,['json']);
+	gulp.watch(htmlSources,['html']);
 });
 
 gulp.task('connect', function(){
@@ -60,4 +66,14 @@ gulp.task('connect', function(){
 	});
 });
 
-gulp.task('default',['coffee','js','sass','connect','watch']);
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('default',['coffee','js','sass','json','connect','watch']);
